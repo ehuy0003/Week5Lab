ec1f0204505ea565476de7e75c1afc0af8d4e57c
Eddie Huynh <ehuy0003@student.monash.edu>
2019-08-27 16:57:43 +1000

    final touches

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 let express = require('express');
+// used to parse the payload of the incoming requests
 let bodyParser = require('body-parser');
-
 let app = express();
 
 let db = [];
 
+// allows Express to understand the urlencodedformat
 app.use(bodyParser.urlencoded({
     extended: false
     })
 )
 
+app.use(bodyParser.json())
+
+// __dirname is the directory name of the current project
 app.use(express.static(__dirname + "/views"));
 app.use(express.static(__dirname + "/img"));
 app.use(express.static(__dirname + "/css"));
 
-
+// allows express to render ejs templates
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-
-
 
 app.get('/', (req,res) => {
     console.log('Welcome to my ToDo Application!!!')
@@ -33,11 +35,14 @@ app.get('/newtask', (req,res) => {
 app.post('/newtask', (req, res) => {
     console.log('POST recieved')
     res.render('index.html')
+    // bodyParser is responsible for generating the body project 
     db.push(req.body)
 })
 
 app.get('/listtask', (req,res) => {
     console.log("Display lists of tasks!!!")
+    // content of the page generated dynamically
+    // copy of the array sent to the rendering engine 
     res.render('listtask.html',{ar:db})
 })
 
